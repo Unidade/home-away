82471d355d28d8042aad7efef922807d7a03bec9
Unidade <dario.rodri123@gmail.com>
2022-12-14 11:44:30 -0300

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -11,4 +15,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
