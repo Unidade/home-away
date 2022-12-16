f027bdfd43adfddd25940d7071175514b4299bd8
Unidade <dario.rodri123@gmail.com>
2022-12-16 15:48:23 -0300

    Change favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -62,7 +62,24 @@ const Layout = ({ children }: ILayoutProps) => {
           name='title'
           content='Fullstack App with Next.js, NextAuth, Supabase & Prisma'
         />
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
       </Head>
 
       <div className='flex min-h-screen flex-col '>
