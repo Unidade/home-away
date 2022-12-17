208e0a8420dc060c57dae0b19b3f38305a6413f5
Unidade <dario.rodri123@gmail.com>
2022-12-17 00:54:35 -0300

    SEO Otimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,35 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <meta
+          name='description'
+          content='HomeAway is a fullstack Airbn-like web application, build with NextJS, NextAuth, TailwindCSS Prisma and Supabase'
+        />
+        <title>HomeAway</title>
+        <meta
+          name='title'
+          content='Fullstack App with Next.js, NextAuth, Supabase & Prisma'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+      </Head>
       <body>
         <Main />
         <NextScript />
