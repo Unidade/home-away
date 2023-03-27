05008062bc0b0b122a3d6ff3a4787fcc04a85d5a
Unidade <dario.rodri123@gmail.com>
2023-03-27 10:35:53 -0300

    Handle error

--- lib/fetchJSON.ts
+++ lib/fetchJSON.ts
@@ -1,5 +1,18 @@
+// Wrapper around fetch that returns the JSON response
 export async function fetchJSON(...args: Parameters<typeof fetch>) {
-  const resp = await fetch(...args)
-  const json = await resp.json()
-  return json
+  try {
+    const resp = await fetch(...args)
+    if (!resp.ok) {
+      throw new Error(resp.statusText)
+    }
+    const json = await resp.json()
+    return json
+  } catch (error) {
+    console.error(error)
+    if (error instanceof Error) {
+      throw error
+    } else {
+      throw new Error('Unknown error', error)
+    }
+  }
 }
