9455c1eb527590d745fb31451aca59ed9c0707a5
Unidade <dario.rodri123@gmail.com>
2023-03-27 10:53:42 -0300

    Colocate useModalHook in Context & Optimization

--- hooks/useModal.ts
+++
@@ -1,8 +0,0 @@
-import { modalContext } from 'context/modalContext'
-import { useContext } from 'react'
-
-export const useModal = () => {
-  const modalStates = useContext(modalContext)
-
-  return { ...modalStates }
-}
