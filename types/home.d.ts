6fe1289906df58a5d622bbb8444a74ea2d6c9b27
Unidade <dario.rodri123@gmail.com>
2023-06-13 22:03:47 -0300

    chore: remove unnecessary type file

--- types/home.d.ts
+++
@@ -1,5 +0,0 @@
-import { Prisma } from '@prisma/client'
-
-// https://www.prisma.io/docs/concepts/components/prisma-client/advanced-type-safety/operating-against-partial-structures-of-model-types
-const homeArgs = Prisma.validator<Prisma.HomeArgs>()
-export type IHome = Prisma.HomeGetPayload<typeof homeArgs>
