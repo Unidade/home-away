d22e37642b0733a690d04b82163be2a6edeb291a
Unidade <dario.rodri123@gmail.com>
2023-06-13 22:10:46 -0300

    chore: add empty space

--- lib/fetchJSON.ts
+++ lib/fetchJSON.ts
@@ -8,13 +8,17 @@ export async function fetchJSON(
 ): Promise<any> {
   try {
     const resp = await fetch(...args)
+
     if (!resp.ok) {
       throw new Error(resp.statusText)
     }
+
     const json = await resp.json()
+
     return json
   } catch (error) {
     console.error(error)
+
     if (error instanceof Error) {
       throw error
     } else {
